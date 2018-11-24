e53ed9dae51223c98368e2453a92ec47b74575ba
Sjors Jessen <sjors.jessen@gmail.com>
2018-11-24 23:19:48 +0100

    Added script to open door

---
+++ EnjoyYourStay/Source/EnjoyYourStay/OpenDoor.h
@@ -0,0 +1,27 @@
+// Copyright Sjors Jessen 2018
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ENJOYYOURSTAY_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;	
+	
+};
